8f4d10b4bc5f180934a9db2cffca322a985bd2b6
umeshukm <umeshukm@gmail.com>
2022-08-04 12:49:58 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -7,8 +7,8 @@ resource "aws_instance" "myawsserver" {
   key_name = "umesh-import"
 
   tags = {
-    Name = "Umesh-DevOps-server"
-    env = "Prod"
+    Name = "Umesh-DevOps-server-Dev"
+    env = "Dev"
     owner = "Umesh"
   }
   provisioner "local-exec" {
